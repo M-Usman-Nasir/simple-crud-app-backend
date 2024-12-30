cd4a811fd996a8af64bcfd8e9467b1713ea00ad2
M-Usman-Nasir <usmannasir98@gmail.com>
2024-12-30 22:38:51 +0500

    updated

--- index.js
+++ index.js
@@ -21,69 +21,6 @@ app.get("/", (req, res) => {
   );
 });
 
-// app.get("/api/products", async (req, res) => {
-//   try {
-//     const products = await Product.find({});
-//     res.status(200).json(products);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.get("/api/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findById(id);
-//     if (!product) {
-//       return res.status(404).json({ message: "Product not found" });
-//     }
-//     res.status(200).json(product);
-//   } catch (error) {
-//     res.status(500).json({ message: "Invalid ID or server error" });
-//   }
-// });
-
-// app.post("/api/products", async (req, res) => {
-//   console.log("Incoming Request Body:", req.body); // Debug log
-//   try {
-//     const product = await Product.create(req.body);
-//     res.status(201).json(product);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// Update
-// app.put("/api/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findByIdAndUpdate(id, req.body, {
-//       new: true,
-//     });
-//     if (!product) {
-//       return res.status(404).json({ message: "Product not found" });
-//     }
-//     res.status(200).json(product);
-//   } catch (error) {
-//     res.status(500).json({ message: "Invalid ID or server error" });
-//   }
-// });
-
-// Delete
-// app.delete("/api/products/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findByIdAndDelete(id);
-//     if (!product) {
-//       return res.status(404).json({ message: "Product not found" });
-//     }
-//     res.status(200).json({ product, message: "Product deleted successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Invalid ID or server error" });
-//   }
-// });
-
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
